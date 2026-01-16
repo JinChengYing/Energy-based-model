70debbcbd9e3fd3f0548bb450b6640c461d33dac
PoorLoserCoder <shuhongrui18@gmail.com>
2026-01-16 16:13:23 +1100

    0.2

---
+++ CNN.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+
+class CNNModel(nn.Module):
+
+    def __init__(self, hidden_features=32,out_dim=1,**kwargs):
+        super().__init__()
+        # We increase the hidden dimension over layers. Here pre-calculated for simplicity.
+        c_hid1 = hidden_features//2 #设置CNN网络结构中隐藏层的不同宽度
+        c_hid2 = hidden_features
+        c_hid3 = hidden_features*2
+
+        # Series of convolutions and Swish activation functions
+        #卷积结构
+        self.cnn_layers = nn.Sequential(
+            nn.Conv2d(1,c_hid1,kernel_size=5,stride=2,padding=4),
+            Swish(),
+            nn.Conv2d(c_hid1, c_hid2,kernel_size=3, stride=5, padding=1),
+            Swish(),
+            nn.Conv2d(c_hid2,c_hid3 ,kernel_size=3, stride=2, padding=1),
+            Swish(),
+            nn.Conv2d(c_hid3,c_hid3, kernel_size=3, stride=2, padding=1),
+            Swish(),
+            nn.Flatten(),#展平网络输出
+            nn.Linear(c_hid3*4,c_hid3),
+            Swish,
+            nn.Linear(c_hid3,out_dim)
+        )
+
+    def forward(self,x):
+        x = self.cnn_layers(x).squeeze(dim=-1)
+        return x
+
+
+
+
+
+
+    #前向
+
---
+++ data_loader.py
@@ -0,0 +1,31 @@
+#主要做训练集的处理
+#将数据集转化为tensor, 并且正则化为-1到1之间.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+# Torchvision
+import torchvision
+from torchvision.datasets import MNIST
+from torchvision import transforms
+
+# Path to the folder where the datasets are/should be downloaded (e.g. CIFAR10)
+DATASET_PATH = "C:/EBM/datasets"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "C:/EBM/module"
+transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,),(0.5,))])
+
+
+#加载训练集,分为训练和验证部分:
+train_set  = MNIST(root=DATASET_PATH,train=True,transform=transform,download=True)
+
+#load test set
+test_set = MNIST(root=DATASET_PATH,train=False,transform=transform,download=True)
+
+#定义数据加载器
+#对于实际的训练模型, 使用不同的数据加载器.
+train_loader = data.DataLoader(train_set, batch_size=128, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
+test_loader= data.DataLoader(test_set, batch_size=256, shuffle=False, drop_last=False, num_workers=4)
+#shuffle:是否打乱顺序.
+
